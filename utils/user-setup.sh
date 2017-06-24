2831b6243f7d2fb31f08850514f3575383ab625a
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-06-24 13:30:33 +0200

    Update user-setup.sh

--- utils/user-setup.sh
+++ utils/user-setup.sh
@@ -1,37 +1,31 @@
-#!/bin/bash -xv
+#!/bin/bash
 #
 # Copyright 2017 - Adriano Pezzuto
 # https://github.com/kalise
 #
+# Make sure the private ssh key ~/.ssh/gcp.pem is present in user home dir
+# Also ~/.ssh/config contains the "IdentityFile ~/.ssh/gcp.pem" string
+#
 sudo groupadd docker
-for i in one two three four five six seven eight nine ten eleven twelve;
+for i in `seq -w 01 12`;
 do
-        USER=user-$i;
-        echo "add user " $USER
+        USER=user$i;
+        echo "add user         " $USER
         sudo useradd $USER
-        echo "add password for user " $USER
-        sudo passwd $USER
         sudo usermod -aG wheel $USER
         sudo usermod -aG docker $USER
+        echo "copy ssh key for " $USER
         sudo mkdir /home/$USER/.ssh
         sudo cp ~/.ssh/authorized_keys /home/$USER/.ssh/authorized_keys
         sudo chmod 700 /home/$USER/.ssh
         sudo chmod 600 /home/$USER/.ssh/authorized_keys
         sudo chown $USER:$USER /home/$USER/.ssh
         sudo chown $USER:$USER /home/$USER/.ssh/authorized_keys
-
+        echo "copy GCP key for " $USER
         sudo cp ~/.ssh/gcp.pem /home/$USER/.ssh/gcp.pem
         sudo cp ~/.ssh/config /home/$USER/.ssh/config
         sudo chown $USER:$USER /home/$USER/.ssh/gcp.pem
         sudo chown $USER:$USER /home/$USER/.ssh/config
         sudo chmod 600 /home/$USER/.ssh/config
         sudo chmod 600 /home/$USER/.ssh/gcp.pem
-
-        sudo cp ./kube-context-setup.sh /home/$USER/kube-context-setup.sh
-        sudo chown $USER:$USER /home/$USER/kube-context-setup.sh
-        sudo chmod u+x /home/$USER/kube-context-setup.sh
-        sudo su -c "/home/$USER/kube-context-setup.sh" -s /bin/bash $USER
 done
-
-# Make sure the private ssh key ~/.ssh/gcp.pem is present in user home dir
-# Also ~/.ssh/config contains the "IdentityFile ~/.ssh/gcp.pem" string

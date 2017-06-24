cae9695eb5b3c573d63279deda5c95b152d71c29
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-06-24 13:32:25 +0200

    Update kube-context-setup.sh

--- utils/kube-context-setup.sh
+++ utils/kube-context-setup.sh
@@ -6,14 +6,15 @@
 # Create context for kubectl
 #
 USER=$(whoami)
-NAMESPACE=project-${USER:5}
-CLUSTER=aws-cluster
-SERVER=http://kube-master:8080
-kubectl create namespace $NAMESPACE
-kubectl label namespace $NAMESPACE type=project
+NAMESPACE=project${USER:4:2}
+CLUSTER=kubernetes
+SERVER=http://10.10.10.90:8080
 kubectl config set-credentials $USER
 kubectl config set-cluster $CLUSTER --server=$SERVER
 kubectl config set-context $NAMESPACE/$CLUSTER/$USER --cluster=$CLUSTER --user=$USER
 kubectl config set contexts.$NAMESPACE/$CLUSTER/$USER.namespace $NAMESPACE
 kubectl config use-context $NAMESPACE/$CLUSTER/$USER
+kubectl create namespace $NAMESPACE
+kubectl label namespace $NAMESPACE type=project
 kubectl get all -n $NAMESPACE
+

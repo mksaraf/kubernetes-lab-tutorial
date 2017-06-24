47c1bb4b11d77bbebd0c63404a72b10a26810df9
Adriano Pezzuto <kalise@users.noreply.github.com>
2017-06-24 13:31:12 +0200

    Update flush-all-users.sh

--- utils/flush-all-users.sh
+++ utils/flush-all-users.sh
@@ -1,10 +1,11 @@
-#!/bin/bash -xv
+#!/bin/bash
 #
 # Copyright 2017 - Adriano Pezzuto
 # https://github.com/kalise
 #
-for i in one two three four five six seven eight nine ten eleven twelve;
+for i in `seq -w 01 12`;
 do
-  USER=user-$i;
+  USER=user$i;
+  echo "deleting user         " $USER
   sudo userdel -rf $USER
 done

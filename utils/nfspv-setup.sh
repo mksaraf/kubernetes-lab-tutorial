1e835e06feaf072475f0f7b1bcc97885e352e1f0
Adriano Pezzuto <kalise@users.noreply.github.com>
2018-05-25 20:17:45 +0200

    Update nfspv-setup.sh

--- utils/nfspv-setup.sh
+++ utils/nfspv-setup.sh
@@ -4,7 +4,7 @@
 # https://github.com/kalise
 #
 # Setup NFS persistent volumes on kubernetes
-for i in `seq -w 00 09`;
+for i in `seq -w 01 09`;
 do
 echo $i;
 sed s/00/$i/g nfs-persistent-volume.yaml | kubectl create -f -;

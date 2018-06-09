56e81b9b8b58f35ccf31b0ca780441917f549908
Adriano Pezzuto <kalise@users.noreply.github.com>
2018-06-09 02:43:57 +0200

    Update users-delete.sh

--- utils/users-delete.sh
+++ utils/users-delete.sh
@@ -11,14 +11,21 @@ ZONE=europe-west1-c
 CLUSTER=kube
 gcloud config set compute/region $REGION
 gcloud config set compute/zone $ZONE
+echo "Get user/password credentials to work as cluster administrator"
+gcloud container clusters get-credentials $CLUSTER
+USERNAME=$(gcloud container clusters describe $CLUSTER | grep username | awk '{print $2}')
+PASSWORD=$(gcloud container clusters describe $CLUSTER | grep password | awk '{print $2}')
+kubectl config set-credentials $USERNAME --username=$USERNAME --password=$PASSWORD
+kubectl config set-context $(kubectl config current-context) --namespace=default --user=$USERNAME
+echo
 echo "Delete cluster-wide RBAC authorizations for all authenticated users"
 kubectl delete rolebinding pods-viewer-all --namespace=kube-system
 kubectl delete role pods-viewer --namespace=kube-system
 kubectl delete clusterrolebinding nodes-viewer-all volumes-viewer-all storage-classes-viewer-all
 kubectl delete clusterrole nodes-viewer volumes-viewer storage-classes-viewer
-for i in `seq -w 00 11`;
+for i in `seq -w 00 01`;
 do
-   USER=user$i;
+   USER=noverit$i;
    echo
    echo "====================================================================="
    echo "delete " $USER
@@ -28,7 +35,7 @@ do
    gcloud iam service-accounts delete $USER@$PROJECT.iam.gserviceaccount.com
    
    echo "Delete namespace"
-   NAMESPACE=tenant${USER:4:2}
+   NAMESPACE=project${USER:7:2}
    kubectl delete ns $NAMESPACE
    
    echo "Remove the user and his own home dir"

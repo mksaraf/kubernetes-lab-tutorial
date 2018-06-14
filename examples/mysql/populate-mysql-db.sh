4fe287be267711ce2776608aa5fb2b13bcc562dc
Adriano Pezzuto <kalise@users.noreply.github.com>
2018-06-14 01:38:00 +0100

    Delete populate-mysql-db.sh

--- examples/mysql/populate-mysql-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mysql -h mysql -P 30006 -u root -e "CREATE DATABASE mydatabase CHARACTER SET utf8 COLLATE utf8_general_ci;"
-sleep 5;
-mysql -h mysql -P 30006 -u root -D mydatabase -e "CREATE TABLE IF NOT EXISTS `logs` (`id` int(11) NOT NULL AUTO_INCREMENT, `content` longtext COLLATE utf8_unicode_ci, PRIMARY KEY (`id`)) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;"
-sleep 5;
-for i in `seq 1 32000`; \
-    do mysql -h mysql -P 30006 -u root -D mydatabase -e \
-    "INSERT INTO logs (content) VALUES ('`tail -n 1000 /var/log/messages`')"; \
-done

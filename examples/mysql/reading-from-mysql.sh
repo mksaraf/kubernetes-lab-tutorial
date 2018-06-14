a67c1af96ad6de65b0d578263d1f15f1a05834e7
Adriano Pezzuto <kalise@users.noreply.github.com>
2018-06-14 01:36:17 +0100

    Update reading-from-mysql.sh

--- examples/mysql/reading-from-mysql.sh
+++ examples/mysql/reading-from-mysql.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# We assume the master mysql-0 has been loaded with a test database
+# https://github.com/datacharmer/test_db.git
 while true;
 do
+    echo "Sending next query"
     sleep 2; 
-    mysql -h kubew01 -P 30004 -u root -e 'SELECT @@server_id,NOW()'; 
+    mysql -h mysql-read -D employees -u root -ppassword -e 'SELECT @@server_id,NOW(); SELECT COUNT(*) FROM employees'
     echo ""
 done

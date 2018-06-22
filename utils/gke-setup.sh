1b723888b1188166608a8200f8e1f193271d644e
Adriano Pezzuto <kalise@users.noreply.github.com>
2018-06-22 16:48:52 +0200

    Update gke-setup.sh

--- utils/gke-setup.sh
+++ utils/gke-setup.sh
@@ -2,8 +2,8 @@
 NAME=kube
 PODS_NETWORK_CIDR=10.38.0.0/16
 SERVICES_NETWORK_CIDR=10.32.0.0/16
-CLUSTER_VERSION=1.10.2-gke.3
-NODE_VERSION=1.10.2-gke.3
+CLUSTER_VERSION=1.8.10-gke.0
+NODE_VERSION=1.8.10-gke.0
 DISK_SIZE=100
 IMAGE_TYPE=UBUNTU
 MACHINE_TYPE=n1-standard-1
@@ -12,9 +12,9 @@ SUBNETWORK=kubernetes
 ZONE=europe-west1-c
 NUM_NODES=3
 MAX_NODES=5
-MIN_NODES=3
+MIN_NODES=1
 gcloud container clusters create $NAME \
-    --addons=HttpLoadBalancing,KubernetesDashboard \
+    --addons=KubernetesDashboard \
     --cluster-ipv4-cidr=$PODS_NETWORK_CIDR \
     --enable-ip-alias \
     --cluster-version=$CLUSTER_VERSION \
@@ -32,5 +32,6 @@ gcloud container clusters create $NAME \
     --services-ipv4-cidr=$SERVICES_NETWORK_CIDR \
     --scopes=gke-default,storage-full,monitoring,sql,datastore \
     --username=admin \
+    --enable-autoscaling --min-nodes=$MIN_NODES --max-nodes=$MAX_NODES \
     --no-enable-autorepair
 echo "Cluster provisioned."
